1e30138b2f151b4facd49eda8dfb591b462161f0
Juzo(-.-) <alexs22221@gmail.com>
2024-11-17 15:20:14 +0300

    complete for python

---
+++ Easy/Valid_Parentheses/Valid.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {')':'(', 
+                    '}':'{',
+                    ']':'['
+                    }
+        stack  = []
+
+        for element in s:
+            if element in brackets.values():
+                stack.append(element)
+            elif stack and element in brackets.keys() and stack[-1] == brackets[element]:
+                stack.pop(-1)
+            else:
+                return False
+
+        if stack:
+            return False
+        return True
